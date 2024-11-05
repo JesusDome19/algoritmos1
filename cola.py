0b5a8c7b4878ce9108e5fae3ffee56a8c2043923
gabriel <169124495+JesusDome19@users.noreply.github.com>
2024-11-04 23:31:03 -0300

    clase cola

---
+++ cola.py
@@ -0,0 +1,27 @@
+class Queue:
+
+    def __init__(self):
+        self.__elements = []
+
+    def arrive(self, element):
+        self.__elements.append(element)
+
+    def attention(self):
+        if len(self.__elements) > 0:
+            return self.__elements.pop(0)
+        else:
+            return None
+    
+    def size(self):
+        return len(self.__elements)
+
+    def on_front(self):
+        if len(self.__elements) > 0:
+            return self.__elements[0]
+        else:
+            return None
+    
+    def move_to_end(self):
+        element = self.attention()
+        if element is not None:
+            self.arrive(element)
